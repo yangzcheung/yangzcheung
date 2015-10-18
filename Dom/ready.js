c1499ca053be8e1e950b77ce1db98b03dc309f69
yangzcheung <676896717@qq.com>
2015-10-18 23:58:05 +0800

    added

---
+++ Dom/ready.js
@@ -0,0 +1,14 @@
+function $(fn){
+	if(document.addEventListener){
+		document.addEventListener("DOMContentLoaded",fn,false);
+	} else {
+		document.attachEvent("onreadystatechange",function(){
+			
+			if(document.readyState == "complete"){
+				fn && fn();
+			}	
+			
+		});
+	}
+	
+}
